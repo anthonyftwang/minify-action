19be59018d2426defe437a26c32c698ebe5bb258
Jossy Devers <jossydevers@gmail.com>
2020-10-18 03:49:03 -0400

    Added output path and way to minify a single file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,35 @@
-#!/bin/sh -l
+#!/bin/bash
 
 npm i minify -g
 apt-get update
 apt-get -y install moreutils
 
-find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
-    do
-    minify ${fname} | sponge ${fname}
-    done
+minify_file(){
+    directory=$1
+    basename=$(basename $directory);
+    extension="${basename##*.}"
+    output="";
+    if [ -z "$INPUT_OUTPUT" ]
+    then
+        output="${directory%/*}/"
+    else
+        mkdir -p $INPUT_OUTPUT
+        output="$INPUT_OUTPUT";
+    fi
+    filename="${basename%.*}"
+    output_path="${output}${filename}.min.${extension}"
+    minify ${directory} | sponge ${output_path}
+    echo "minify ${directory} > ${output_path}"
+}
+
+if [ -z "$INPUT_DIRECTORY" ]
+then
+    find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
+        do
+            if [[ "$fname" != *"min."* ]]; then
+                minify_file $fname
+            fi
+        done
+else
+    minify_file $INPUT_DIRECTORY
+fi
